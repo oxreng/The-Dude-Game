936e901ae1642dc6fb2708728475f7def7cbee44
oxreng <merkurev06@mail.ru>
2023-12-28 22:02:09 +0300

    camera features (zoom)

--- cameras.py
+++ cameras.py
@@ -3,52 +3,7 @@ from config import *
 from load_image import load_image
 
 
-class CameraGroup_ysort(pygame.sprite.Group):
-    def __init__(self):
-        super().__init__()
-        self._display_surface = pygame.display.get_surface()
-        # Для фона
-        # self._ground_surf = load_image(..., ...)
-        # self._ground_rect = self._ground_surf.get_rect(topleft=(0, 0))
-
-    def custom_draw(self):
-        # Рисуем фон
-        # self._display_surface.blit(self._ground_surf, self._ground_rect)
-
-        # Обрабатываем объекты
-        for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
-            self._display_surface.blit(sprite.image, sprite.rect)
-
-
-class CameraGroup_center_player(pygame.sprite.Group):
-    def __init__(self):
-        super().__init__()
-        self._display_surface = pygame.display.get_surface()
-
-        # camera offset
-        self.offset = pygame.math.Vector2()
-
-        # Для фона
-        # self._ground_surf = load_image(..., ...)
-        # self._ground_rect = self._ground_surf.get_rect(topleft=(0, 0))
-
-    def center_target_camera(self, target):
-        self.offset.x = target.rect.centerx - HALF_SCREEN_WIDTH
-        self.offset.y = target.rect.centery - HALF_SCREEN_HEIGHT
-
-    def custom_draw(self, *groups, player):
-        self.center_target_camera(player)
-        # Рисуем фон
-        # groind_offset = self._ground_rect - self.offset
-        # self._display_surface.blit(self._ground_surf, groind_offset)
-
-        # Обрабатываем объекты
-        for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
-            offset_pos = sprite.rect.topleft - self.offset
-            self._display_surface.blit(sprite.image, offset_pos)
-
-
-class CameraGroup_box_camera(pygame.sprite.Group):
+class CameraGroup(pygame.sprite.Group):
     def __init__(self):
         super().__init__()
         self._display_surface = pygame.display.get_surface()
@@ -64,13 +19,19 @@ class CameraGroup_box_camera(pygame.sprite.Group):
         h = SCREEN_HEIGHT - top - self._camera_borders['bottom']
         self._camera_rect = pygame.Rect(left, top, w, h)
 
+        # camera zoom
+        self._zoom_scale = 0.8
+        self._internal_surface_size = (2500, 2500)
+        self._internal_surface = pygame.Surface(self._internal_surface_size, pygame.SRCALPHA)
+        self._internal_rect = self._internal_surface.get_rect(center=(HALF_SCREEN_WIDTH, HALF_SCREEN_HEIGHT))
+        self._internal_surface_size_vector = pygame.math.Vector2(self._internal_surface_size)
+        self._internal_offset = pygame.math.Vector2()
+        self._internal_offset.x = self._internal_surface_size[0] // 2 - HALF_SCREEN_WIDTH
+        self._internal_offset.y = self._internal_surface_size[1] // 2 - HALF_SCREEN_HEIGHT
+
         # Для фона
         # self._ground_surf = load_image(..., ...)
         # self._ground_rect = self._ground_surf.get_rect(topleft=(0, 0))
-
-    def center_target_camera(self, target):
-        self._offset.x = target.rect.centerx - HALF_SCREEN_WIDTH
-        self._offset.y = target.rect.centery - HALF_SCREEN_HEIGHT
 
     def box_target_camera(self, target):
         if target.rect.left < self._camera_rect.left:
@@ -84,14 +45,30 @@ class CameraGroup_box_camera(pygame.sprite.Group):
         self._offset.x = self._camera_rect.left - self._camera_borders['left']
         self._offset.y = self._camera_rect.top - self._camera_borders['top']
 
+    def zoom_keyboard_control(self, mouse):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_z] and mouse == 4:
+            self._zoom_scale = min(0.8, self._zoom_scale + 0.1)
+        if keys[pygame.K_z] and mouse == 5:
+            self._zoom_scale = max(0.5, self._zoom_scale - 0.1)
+
     def custom_draw(self, *groups, player):
-        # self.center_target_camera(player)
+        # Меняем координаты всем объектам
         self.box_target_camera(player)
+
+        # Делаем зум
+        self._internal_surface.fill(SKYBLUE)
+
         # Рисуем фон
-        # ground_offset = self._ground_rect - self._offset
-        # self._display_surface.blit(self._ground_surf, ground_offset)
+        # ground_offset = self._ground_rect - self._offset + self._internal_offset
+        # self._internal_surface.blit(self._ground_surf, ground_offset)
 
         # Обрабатываем объекты
         for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
-            offset_pos = sprite.rect.topleft - self._offset
-            self._display_surface.blit(sprite.image, offset_pos)
+            offset_pos = sprite.rect.topleft - self._offset + self._internal_offset
+            self._internal_surface.blit(sprite.image, offset_pos)
+
+        scaled_surf = pygame.transform.scale(self._internal_surface,
+                                             self._internal_surface_size_vector * self._zoom_scale)
+        scaled_rect = scaled_surf.get_rect(center=(HALF_SCREEN_WIDTH, HALF_SCREEN_HEIGHT))
+        self._display_surface.blit(scaled_surf, scaled_rect)
